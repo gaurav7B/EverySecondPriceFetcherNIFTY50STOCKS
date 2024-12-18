edd7cbe500242db853c9965c49e33d6edf54a9af
Gaurav Bhoite <gaurav@velitsolutions.com>
2024-12-18 17:14:11 +0530

    StockTickerExchangeController Added

---
+++ EverySecondPriceFetcherNIFTY50STOCKS/Controllers/StockTickerExchangeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EverySecondPriceFetcherNIFTY50STOCKS.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EverySecondPriceFetcherNIFTY50STOCKS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockTickerExchangeController : ControllerBase
+    {
+        private readonly StockLogger2DbContext _context;
+
+        public StockTickerExchangeController(StockLogger2DbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/StockTickerExchange
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<StockTickerExchange>>> GetStockTickerExchanges()
+        {
+            return await _context.StockTickerExchanges.ToListAsync();
+        }
+    }
+}
